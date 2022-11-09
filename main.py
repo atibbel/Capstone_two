ec49d08833c40516c92b05ef13b80282ab1eea0a
Amber Wonnenberg <81259888+atibbel@users.noreply.github.com>
2022-11-09 00:47:17 +0000

    added system info function

--- main.py
+++ main.py
@@ -9,77 +9,51 @@ class ScoutSystemCheck:
 
     def __init__(self):
         self.system_check = 0
-        self.systems_good = True
         self.system_status = ""
 
-    def antenna_check(self, system_check):
-        if system_check == 0:
-            self.system_status = "FAIL"
-            global fail
-            fail += 1
-        elif system_check == 1:
-            self.system_status = "OK"
-            global ok
-            ok += 1
-        if system_check == 2:
-            self.system_status = "WARN"
-            global warn
-            warn += 1
+    def antenna_check(self):
+        print(f"Checking antennas (L/R):  {self.systems_check(self.system_information())}")
 
-        print(f"Checking antennas (L/R):      [{self.system_status}]")
+    def camera_check(self):
+        print(f"Checking camera:          {self.systems_check(self.system_information())}")
 
-    def camera_check(self, system_check):
-        if system_check == 0:
-            self.system_status = "FAIL"
-            global fail
-            fail += 1
-        elif system_check == 1:
-            self.system_status = "OK"
-            global ok
-            ok += 1
-        if system_check == 2:
-            self.system_status = "WARN"
-            global warn
-            warn += 1
-        print(f"Checking camera:              [{self.system_status}]")
-
-    def arm_check(self, system_check):
-        if system_check == 0:
-            self.system_status = "FAIL"
-            global fail
-            fail += 1
-        elif system_check == 1:
-            self.system_status = "OK"
-            global ok
-            ok += 1
-        if system_check == 2:
-            self.system_status = "WARN"
-            global warn
-            warn += 1
-
-        print(f"Checking arms:                [{self.system_status}]")
+    def arm_check(self):
+        print(f"Checking arms:            {self.systems_check(self.system_information())}")
 
     def torso_check(self):
-        self.system_check
-        return self.system_check
+        print(f"Checking Torso:           {self.systems_check(self.system_information())}")
 
     def leg_check(self):
-        self.system_check = 1
-        return self.system_check
+        print(f"Checking legs:            {self.systems_check(self.system_information())}")
 
     def ai_link(self):
-        self.system_check = 1
-        return self.system_check
+        print(f"Checking connection:      {self.systems_check(self.system_information())}")
 
-    def all_systems_check(self):
-        self.antenna_check()
-        if self.leg_check():
+    def systems_check(self, system_check):
+        if system_check == 0:
+            self.system_status = "[FAIL] SCOUT cannot operate."
+            global fail
+            fail += 1
+        elif system_check == 1:
+            self.system_status = "[OK]"
+            global ok
+            ok += 1
+        if system_check == 2:
+            self.system_status = "[WARN] SCOUT service degraded. Repair or replace soon."
+            global warn
+            warn += 1
+
+        return self.system_status
+
+    def system_information(self):
+        self.system_check = random.randint(0, 100)
+
+        if self.system_check <= 65:
+            return 1
+        elif self.system_check <= 90 :
+            return 2
+        else:
             return 0
-
-        if self.torso_check() and self.camera_check()\
-                and self.camera_check() and self.ai_link():
-
-            return self.all_systems_good
 
 
 scout = ScoutSystemCheck()
@@ -89,10 +63,21 @@ print("SOFTWARE LOADED [OK]")
 
 print("\nBEGIN BOOT SEQUENCE CHECKS"
       "\n--------------------------")
-scout.antenna_check(random.randint(0, 2))
-scout.camera_check(random.randint(0, 2))
-scout.arm_check(random.randint(0, 2))
+scout.antenna_check()
+scout.camera_check()
+scout.arm_check()
+scout.torso_check()
+scout.leg_check()
+scout.ai_link()
 
 print(f"\nChecks completed with {ok} [OK]  {warn} [WARN]  {fail} [FAIL]")
+
+if fail == 0:
+    print("SCOUT IS OPERATIONAL")
+else:
+    print("SCOUT CANNOT OPERATE. SHUTTING DOWN...")
+
+
+
 
 
