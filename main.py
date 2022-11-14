ad0658011b794351239868388424fad1dceff139
Alexander Muns <109362034+a-muns@users.noreply.github.com>
2022-11-14 17:24:48 +0000

    Added comments for clarification, some edits to match format with Colby's example.

--- main.py
+++ main.py
@@ -15,35 +15,35 @@ class ScoutSystemCheck:
   # These methods each get passed a random status as defined in system_information()
   def antenna_check(self):
     print(
-      f"Checking antennas (L/R):  {self.systems_check(self.system_information())}"
+      f"Checking antennas (L/R):\t\t   {self.systems_check(self.system_information())}"
     )
 
   def camera_check(self):
     print(
-      f"Checking camera:          {self.systems_check(self.system_information())}"
+      f"Checking camera:\t\t           {self.systems_check(self.system_information())}"
     )
 
   def arm_check(self):
     print(
-      f"Checking arms:            {self.systems_check(self.system_information())}"
+      f"Checking arms:\t\t               {self.systems_check(self.system_information())}"
     )
 
   def torso_check(self):
     print(
-      f"Checking torso:           {self.systems_check(self.system_information())}"
+      f"Checking torso:\t\t               {self.systems_check(self.system_information())}"
     )
 
   def leg_check(self):
     print(
-      f"Checking legs:            {self.systems_check(self.system_information())}"
+      f"Checking legs:\t\t               {self.systems_check(self.system_information())}"
     )
 
   def ai_link(self):
     print(
-      f"Checking connection:      {self.systems_check(self.system_information())}"
+      f"Checking connection:\t\t       {self.systems_check(self.system_information())}"
     )
 
-  # Print status and update globals based on parameter passed in
+  # Print status and update globals based on parameter passed
   def systems_check(self, system_check):
     if system_check == 0:
       self.system_status = "[FAIL] SCOUT cannot operate."
@@ -60,7 +60,7 @@ class ScoutSystemCheck:
 
     return self.system_status
 
-  # Return random status; Chances: [OK] 65% | [WARN] 25% | [FAIL] 10%
+  # Return random status. Chances: [OK] 65% | [WARN] 25% | [FAIL] 10%
   def system_information(self):
     self.system_check = random.randint(0, 100)
 
@@ -74,7 +74,7 @@ class ScoutSystemCheck:
 
 # Execution
 scout = ScoutSystemCheck()
-print("\n\nStarting up ...")
+print("\n\nStarting up....")
 print("FIRMWARE LOADED [OK]")
 print("SOFTWARE LOADED [OK]")
 
@@ -87,9 +87,9 @@ scout.torso_check()
 scout.leg_check()
 scout.ai_link()
 
-print(f"\nChecks completed with {ok} [OK]  {warn} [WARN]  {fail} [FAIL]")
+print(f"\nChecks completed with: {ok} [OK]  {warn} [WARN]  {fail} [FAIL]")
 
 if fail == 0:
-  print("SCOUT IS OPERATIONAL")
+  print("SCOUT IS OPERATIONAL.")
 else:
   print("SCOUT CANNOT OPERATE. SHUTTING DOWN...")
